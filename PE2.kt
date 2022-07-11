4dd0383a516ff7d19d51bd4a4cc21d6ba7bd494c
rkoum <95508360+rkoum@users.noreply.github.com>
2022-07-12 00:30:07 +0300

    Create PE2.kt

---
+++ PE2.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val lista = mutableListOf<Int>()
+    var i = 2
+    lista.add(0, 0)
+    lista.add(1, 1)
+
+    while (lista[i - 1] + lista[i - 2] < 4000000) {
+        lista.add(i, lista[i - 1] + lista[i - 2])
+        i++
+    }
+    lista.filter { x -> x % 2 == 0 }
+    val lista2 = lista.filter { x -> x % 2 == 0 }
+    println(lista2.sum())
+}
