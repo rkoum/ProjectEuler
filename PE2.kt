cecc63dc1092cebf05f102582acfa75a4e4a12e8
rkoum <95508360+rkoum@users.noreply.github.com>
2022-07-12 18:21:22 +0300

    Update PE2.kt

--- PE2.kt
+++ PE2.kt
@@ -8,6 +8,6 @@ fun main() {
         lista.add(i, lista[i - 1] + lista[i - 2])
         i++
     }
-    lista.filter { x -> x % 2 == 0 }
+    val lista2=lista.filter { x -> x % 2 == 0 }
     println(lista2.sum())
 }
