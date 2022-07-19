858d930c18795d8afa55a5bccb8fa6209dd969b5
rkoum <95508360+rkoum@users.noreply.github.com>
2022-07-20 00:15:08 +0300

    Update PE9.kt

--- PE9.kt
+++ PE9.kt
@@ -6,7 +6,7 @@ fun main() {
     for (a in 1..1000) {
         for (b in (a + 1)..1000) {
             c = 1000 - a - b
-            if (a + b + c == 1000 && a * a + b * b == c * c) {
+            if ( a * a + b * b == c * c) {
                 println(a * b * c)
             }
         }
